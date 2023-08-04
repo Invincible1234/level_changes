92c99aa4e6ca12144d27b117bef11dc313bf6a06
The-Frige <21050516+The-Frige@users.noreply.github.com>
2023-08-04 01:28:53 -0700

    Added in file loading and made levels saved as files. Also fixed a bug with sprite position.

--- Main.py
+++ Main.py
@@ -4,11 +4,12 @@ from Level import Level
 
 App = App("Shotformer", 640, 480)
 
-Object1 = Object(25, 25, 150, 200, "blue_square.png")
-Object2 = Object(10, 10, 100, 100, "red_square.png")
+#Object1 = Object(25, 25, 150, 200, "blue_square.png")
+#Object2 = Object(10, 10, 100, 100, "red_square.png")
 Level1 = Level()
-Level1.add_obj(Object1)
-Level1.add_obj(Object2)
+Level1.load_level("Test.json")
+#Level1.add_obj(Object1)
+#Level1.add_obj(Object2)
 
 App.app_add_level(Level1)
 #App.app_add_obj(Object1)
